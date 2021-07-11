51cc440708436615d8898a0bca36a883f9b6ca1c
Adam Ploof <adamploof@hotmail.com>
2021-07-11 19:02:12 -0400

    added artwork and collections models, migrations

--- CoSSandbox/artistMarket/models.py
+++ CoSSandbox/artistMarket/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ArtworkCollection(models.Model):
+    collection_name = models.CharField(max_length=40)
+
+    
+class ArtWork(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_date = models.DateField
+    published_date = models.DateField
+    artist = models.ForeignKey(User, on_delete=models.CASCADE)
+    collection = models.ManyToManyField(ArtworkCollection)
+    image = models.CharField(max_length=200)
