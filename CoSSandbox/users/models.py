bbadab0656d2b44bbf6e9b984c876cefb75f9273
Adam Ploof <adamploof@hotmail.com>
2021-07-14 23:06:28 -0400

    extended user model with Profile, create & save profile on user creation

--- CoSSandbox/users/models.py
+++ CoSSandbox/users/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Profile(models.Model):
-    style = models.CharField(max_length=50)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    style = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+ 
